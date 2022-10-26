7659f253bcedb3330c84e542d2979f2ef995fd8a
Kasumiru <kasumiru@gmail.com>
2022-10-27 02:14:05 +0300

    Update deploy_cloudflate_certbot_systemctl_nginx_SSLcert_telegram_bot.sh

--- deploy_cloudflate_certbot_systemctl_nginx_SSLcert_telegram_bot.sh
+++ deploy_cloudflate_certbot_systemctl_nginx_SSLcert_telegram_bot.sh
@@ -115,7 +115,7 @@ certbot certonly --dns-cloudflare --dns-cloudflare-credentials /root/.certbot/cl
 
 
 
-# install latext nginx
+# install latest nginx
 sudo apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
     | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
